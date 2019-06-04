85ce09efcc58e57a282634a03431817e089eef84
Mike Liebold <mliebold95@gmail.com>
2019-06-03 22:36:57 -0400

    End of Chapter 4: React State

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-const issues = [
+const initialIssues = [
   {
     id: 1,
     status: "New",
@@ -25,63 +25,108 @@ class IssueFilter extends React.Component {
   }
 }
 
-class IssueRow extends React.Component {
-  render() {
-    const issue = this.props.issue;
-    return (
-      <tr>
-        <td>{issue.id}</td>
-        <td>{issue.status}</td>
-        <td>{issue.owner}</td>
-        <td>{issue.created.toDateString()}</td>
-        <td>{issue.effort}</td>
-        <td>{issue.due ? issue.due.toDateString() : ""}</td>
-        <td>{issue.title}</td>
-      </tr>
-    );
-  }
+function IssueRow(props) {
+  const issue = props.issue;
+  return (
+    <tr>
+      <td>{issue.id}</td>
+      <td>{issue.status}</td>
+      <td>{issue.owner}</td>
+      <td>{issue.created.toDateString()}</td>
+      <td>{issue.effort}</td>
+      <td>{issue.due ? issue.due.toDateString() : ""}</td>
+      <td>{issue.title}</td>
+    </tr>
+  );
 }
-class IssueTable extends React.Component {
-  render() {
-    const issueRows = issues.map(issue => (
-      <IssueRow key={issue.id} issue={issue} />
-    ));
 
-    return (
-      <table className='bordered-table'>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Status</th>
-            <th>Owner</th>
-            <th>Created</th>
-            <th>Effort</th>
-            <th>Due Date</th>
-            <th>Title</th>
-          </tr>
-        </thead>
-        <tbody>{issueRows}</tbody>
-      </table>
-    );
-  }
+function IssueTable(props) {
+  const issueRows = props.issues.map(issue => (
+    <IssueRow key={issue.id} issue={issue} />
+  ));
+
+  return (
+    <table className='bordered-table'>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Status</th>
+          <th>Owner</th>
+          <th>Created</th>
+          <th>Effort</th>
+          <th>Due Date</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>{issueRows}</tbody>
+    </table>
+  );
 }
 
 class IssueAdd extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = document.forms.issueAdd;
+    const issue = {
+      owner: form.owner.value,
+      title: form.title.value,
+      status: "New"
+    };
+    this.props.createIssue(issue);
+    form.owner.value = "";
+    form.title.value = "";
+  }
+
   render() {
-    return <div>This is a placeholder for a form to add an issue.</div>;
+    return (
+      <form name='issueAdd' onSubmit={this.handleSubmit}>
+        <input type='text' name='owner' placeholder='Owner' />
+        <input type='text' name='title' placehodler='Title' />
+        <button>Add</button>
+      </form>
+    );
   }
 }
 
 class IssueList extends React.Component {
+  constructor() {
+    super();
+    this.state = { issues: [] };
+    this.createIssue = this.createIssue.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData() {
+    setTimeout(() => {
+      this.setState({ issues: initialIssues });
+    }, 500);
+  }
+
+  createIssue(issue) {
+    issue.id = this.state.issues.length + 1;
+    issue.created = new Date();
+    const newIssueList = this.state.issues.slice();
+    newIssueList.push(issue);
+    this.setState({ issues: newIssueList });
+  }
+
   render() {
     return (
       <React.Fragment>
         <h1>Issue Tracker</h1>
         <IssueFilter />
         <hr />
-        <IssueTable />
+        <IssueTable issues={this.state.issues} />
         <hr />
-        <IssueAdd />
+        <IssueAdd createIssue={this.createIssue} />
       </React.Fragment>
     );
   }
@@ -90,27 +135,3 @@ class IssueList extends React.Component {
 const element = <IssueList />;
 
 ReactDOM.render(element, document.getElementById("contents"));
-
-// Create Element Using JSX
-/*const element = (
-  <div title='Outer div'>
-    <h1>Hello World!</h1>
-  </div>
-);
-
-ReactDOM.render(element, document.getElementById("contents"));
-*/
-
-// End of chapter 2 code
-/*const continents = ["Africa", "America", "Asia", "Australia", "Europe"];
-const helloContinents = Array.from(continents, c => `Hello ${c}!`);
-const message = helloContinents.join(" ");
-
-const element = (
-  <div title='Outer div'>
-    <h1>{message}</h1>
-  </div>
-);
-
-ReactDOM.render(element, document.getElementById("contents"));
-*/
